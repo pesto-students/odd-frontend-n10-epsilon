76dbaf53da84dfe5c096946d5fd15354b50579c8
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2021-11-12 00:29:14 +0530

    add box-shadow largest

--- packages/components/tailwind.config.js
+++ packages/components/tailwind.config.js
@@ -170,6 +170,7 @@ module.exports = {
       lg: '0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05)',
       xl: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)',
       '2xl': '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      'largest': '0 4px 4px 0 rgba(0, 0, 0, .20), 0 8px 8px 0 rgba(0, 0, 0, .10)',
       inner: 'inset 0 2px 4px 0 rgba(0, 0, 0, 0.06)',
       none: 'none',
     },
