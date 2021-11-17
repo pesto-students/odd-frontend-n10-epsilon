26f0741db76d9e1b78cc053a2d06be0f9d2d8d8c
DharmendraKumar Jagodana <jagodana.dharmendra@gmail.com>
2021-11-17 23:22:14 +0530

    fix: export of DataTable

--- packages/components/src/molecules/index.ts
+++ packages/components/src/molecules/index.ts
@@ -3,7 +3,7 @@ export { default as SteppedAddresses } from "./address/SteppedAddresses";
 export { default as LabeledIcon } from "./labeled-icon/LabeledIcon";
 export { default as DocUploadListItem } from "./doc-upload/DocUploadListItem";
 export { default as OTPItem } from "./otp/OTPItem";
-export * from "./table/DataTable";
+export { default as DataTable } from "./table/DataTable";
 export { default as AvatarItem } from "./statistics/AvatarItem";
 export { default as StatusItem } from "./statistics/StatusItem";
 export { default as TextItem } from "./statistics/TextItem";
