b7a02a59e55e2d55a939849c0e9ac844e3df5f9f
Akash Malviya <52854186+Akashmalviya@users.noreply.github.com>
2021-11-17 23:43:20 +0530

    dummy redux store setup with Redux-tool-kit

--- packages/admin/src/index.tsx
+++ packages/admin/src/index.tsx
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
 import { BrowserRouter as Router } from "react-router-dom";
+import store from "./redux/store";
 
 ReactDOM.render(
-  <Router>
-    <App />
-  </Router>,
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
   document.getElementById("root")
 );
 
