64d2b646efe5288cf00a12a568c0502ac64098fc
Akash Malviya <52854186+Akashmalviya@users.noreply.github.com>
2021-11-19 23:48:07 +0530

    Driver & User Layout ready

---
+++ packages/driver/src/routes/Routes.tsx
@@ -0,0 +1,34 @@
+import { Routes, Route, Outlet, Navigate } from "react-router-dom";
+import { Dashboard } from "../pages/dashboard/Dashboard";
+import { AuthProvider, RequireAuth } from "../pages/login/AuthProvide";
+import { LoginPage } from "../pages/login/Login";
+
+export default function AppRoutes() {
+  return (
+    <AuthProvider>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route path="/" element={<Navigate to="/login" />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/dashboard"
+            element={
+              <RequireAuth>
+                <Dashboard />
+              </RequireAuth>
+            }
+          >
+            <Route index element={<h1>Dashboard</h1>} />
+            <Route path="driver" element={<h1>Driver</h1>} />
+            <Route path="user" element={<h1>User</h1>} />
+            <Route path="order" element={<h1>Order</h1>} />
+          </Route>
+        </Route>
+      </Routes>
+    </AuthProvider>
+  );
+}
+
+function Layout() {
+  return <Outlet />;
+}
