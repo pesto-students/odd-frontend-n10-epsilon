69394a7a9da33837dfd04bd3620399905e813bfb
Akash Malviya <52854186+Akashmalviya@users.noreply.github.com>
2021-11-21 23:00:38 +0530

    Update: delete duplicate icons file and add index.ts

--- packages/components/src/atoms/index.ts
+++ packages/components/src/atoms/index.ts
@@ -1,4 +1,5 @@
 export  * from "./button";
+export * from "./icon";
 export { default as Switch } from "./switch/Switch";
 export { default as Label } from "./label/Label";
 export { default as Input } from "./input/Input";
