05a506b7c7f673198c5454b15d44de53547c1cab
Akash Malviya <52854186+Akashmalviya@users.noreply.github.com>
2021-11-22 13:06:32 +0530

    update:add index file (#22)

--- packages/components/src/atoms/input/Input.tsx
+++ packages/components/src/atoms/input/Input.tsx
@@ -1,55 +1,59 @@
-import React, { useState, InputHTMLAttributes } from "react";
+import React, { InputHTMLAttributes } from "react";
+import { useState } from "react";
 
 //Components
 import { Label } from "..";
 
 interface IProps extends InputHTMLAttributes<HTMLInputElement> {
-  name?: string;
   label?: string;
   placeholder?: string;
   textType?: string;
   error?: string;
   leading?: React.ReactNode;
+  fieldStyle?: "standard" | "legacy";
   trailing?: React.ReactNode;
+  secure?: boolean;
   className?: string;
   labelClassName?: string;
   style?: React.CSSProperties;
   onFocus?(): void;
-  defaultValue?: string;
 }
 
 const Input: React.FC<IProps> = (props: IProps & any) => {
   const {
-    name,
     label,
     placeholder,
-    textType,
+    fieldStyle,
     error,
     leading,
-    trailing,
-    className,
     labelClassName,
+    trailing,
+    // secure = false,
     style,
-    defaultValue,
-    onChange,
+    className,
+    required,
+    ...rest
   } = props;
 
   const [focused, setFocus] = useState(false);
 
   const set_focus = () => setFocus(true);
   const set_blur = () => setFocus(false);
-  const [value, setValue] = useState(defaultValue);
 
-  return (
+  const standard = (
     <div className={` block gap-2  ${className}`} style={style}>
       {label && (
-        <Label
-          className={labelClassName}
-          block
-          secondary
-          title={label}
-          medium
-        />
+        <>
+          <Label
+            className={labelClassName}
+            block
+            secondary
+            
+            required={required}
+            title={label}
+            medium
+          />
+        </>
       )}
       <div
         className={`flex border  rounded w-full  py-2  
@@ -62,15 +66,9 @@ const Input: React.FC<IProps> = (props: IProps & any) => {
           placeholder={placeholder}
           className="w-full px-2 focus:border-0 focus:ring-0 active:border-0 active:ring-0 focus:outline-none"
           onFocus={set_focus}
+          required={required}
           onBlur={set_blur}
-          type={textType}
-          value={value || ""}
-          onChange={(e) => {
-            setValue(e.target.value);
-            if (onChange) {
-              onChange(e);
-            }
-          }}
+          {...rest}
         />
         {trailing && <div className="mx-2">{trailing}</div>}
       </div>
@@ -81,6 +79,48 @@ const Input: React.FC<IProps> = (props: IProps & any) => {
       )}
     </div>
   );
+  const legacy = (
+    <div className={` block gap-2  ${className}`} style={style}>
+      {label && (
+        <Label
+          className={focused ? "text-primary" : "text-gray"}
+          block
+          title={label}
+          medium
+        />
+      )}
+      <div
+        className={`flex border-b   w-full  py-2  
+        ${focused ? "border-primary" : "border-gray"}
+       
+           `}
+      >
+        {leading && <div className="mx-2 flex-initial ">{leading}</div>}
+        <input
+          placeholder={placeholder}
+          className="w-full px-2 focus:border-0 focus:ring-0 active:border-0 active:ring-0 focus:outline-none"
+          onFocus={set_focus}
+          onBlur={set_blur}
+          {...rest}
+        />
+        {trailing && <div className="mx-2">{trailing}</div>}
+      </div>
+      {error && (
+        <p className="text-xs italic" style={{ color: "#FF0000" }}>
+          {error}
+        </p>
+      )}
+    </div>
+  );
+  switch (fieldStyle) {
+    case "standard":
+      return standard;
+    case "legacy":
+      return legacy;
+
+    default:
+      return standard;
+  }
 };
 
 export default Input;
