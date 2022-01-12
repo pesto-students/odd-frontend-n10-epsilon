2bbb4d867f366a5930d5c61d0466be64f44c94fb
DharmendraKumar Jagodana <47411494+JagodanaDharmendra@users.noreply.github.com>
2022-01-12 23:40:31 +0530

    Update: add skeleton and scroll to Trips-History (#64)

--- packages/driver/src/pages/trips-payments/components/index.ts
+++ packages/driver/src/pages/trips-payments/components/index.ts
@@ -1,3 +1,4 @@
 export { default as OrderProgressItem } from "./OrderProgressItem"
 export { default as StatisticsItem } from "./StatisticsItem"
 export { default as OrderItem } from "./OrderItem"
+export { default as Skeleton } from "./Skeleton"
